f5f402348d04cd73a07b9f88f642988023aeeac4
Alexandre <57919367+Zer67@users.noreply.github.com>
2020-10-27 00:04:14 +0100

    css pour les pages d'inscription et de log
    J'ai ajouté une feuille css récupérée d'un ancien projet. Je m'en suis servi de base pour construire le css de ce projet. J'ai essentiellement travaillé avec des classes pour faire la text_box et le fond d'écran. Pour ce qui est du texte et des boutons j'ai modifié les instances en elles-mêmes pour les premières et créé une classe pour les secondes.

--- create_account.php
+++ create_account.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<link rel="stylesheet" type="text/css" href="style_site.css">
+<html lang="en" dir="ltr" class="clogin">
   <head>
     <meta charset="utf-8">
     <title>Inscription</title>
   </head>
-  <body>
+  <body class="text_box">
+  <div class="line">
     <h1>Inscription au site </h1>
-    <p>Entre tes informations ci dessous<p/><br/>
-    <form>
+    <p class="white_text">Entre tes informations ci dessous<p/><br/>
+    <form class="white_text">
 	    <label for="name">Name</label>
 	    <input name="name" type="text" value="Frank" /> <br /><br />
 
@@ -23,5 +25,6 @@
   	  <label for="name">Name</label>
 	    <input name="name" type="text" value="Frank" /> <br /><br />
     </form>
+  </div>
   </body>
 </html>
--- login.php
+++ login.php
@@ -1,37 +1,40 @@
 <!DOCTYPE html>
 <?php
-  session_start();
-  if(isset($_SESSION['login']) && $_SESSION['login'] == true){
+session_start();
+if(isset($_SESSION['login']) && $_SESSION['login'] == true){
     header('location: home.php');
-  }
+}
 ?>
-<html lang="fr">
-  <head>
+<html lang="fr" class="clogin">
+<link rel="stylesheet" type="text/css" href="style_site.css">
+<head>
     <meta charset="utf-8">
     <title>Login</title>
-  </head>
-  <body>
+</head>
+<body class="text_box">
+<div class="line">
     <h1>Bienvenue sur UnTrollBienMécontent.fr</h1>
-    <p>
-      Discutez de vos jeux favoris, avec une communauté active et sympathique !
+    <p class="white_text">
+        Discutez de vos jeux favoris, avec une communauté active et sympathique !
     </p>
-    <br/>
+    <br/><br>
+    <form class="white_text" method ="post" action="check_login.php">
+        <label for="uname">Username:</label>
+        <input type="text" name="uname" placeholder="Username" required>
 
-    <form method="post" action="check_login.php">
-      <label for="uname">Username:</label>
-      <input type="text" name="uname" placeholder="Username" required><br/>
+        <label for="pwd">Password:</label>
+        <input type="password" name="pwd" placeholder="Password" required>
 
-      <label for="pwd">Password:</label>
-      <input type="password" name="pwd" placeholder="Password" required><br/>
-
-      <button type="submit">Login</button>
+        <button type="submit" class="bouton_log_sign">Login</button>
 
     </form>
     <?php
-      if(isset($_GET['failed'])){
+    if(isset($_GET['failed'])){
         echo("<br/>Connection failed, please try again :(<br/>");
-      }
-     ?>
-    <p>Pas encore memebre ? <a href="create_account.php">Inscrivez-vous ici !</a></p>
-  </body>
+    }
+    ?>
+
+    <p class="white_text"><br>Pas encore membre ? <a href="create_account.php">Inscrivez-vous ici !</a></p>
+</div>
+</body>
 </html>
