729d8dc881bf1fab79cacf2b104448e619cf2b37
LeoChaillard <leochaillard@gmail.com>
2020-10-28 20:36:18 +0100

    Search system partially done

--- home.php
+++ home.php
@@ -5,10 +5,17 @@
       <title>Homepage</title>
     </head>
     <body>
-      <p>
       <?php
         include 'header.php';
       ?>
-      </p>
+      <form method="post" action="search.php" >
+
+      <br/><br/><label for="recherche"></label>
+  	  <input name="recherche" type="text" placeholder="Rechercher un jeu"/><br /><br/>
+      <button type="submit">Rechercher</button>
+
+
+    </form>
+
     </body>
 </html>
---
+++ search.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Critiques</title>
+  </head>
+  <body>
+    <?php
+      include 'header.php';
+      ?>
+    </br></br><h1>Résultats</h1>
+    <h1>Filtres</h1>
+    Trier par :
+    <INPUT type="checkbox" name="filtre" value="nom"> nom
+    <INPUT type="checkbox" name="filtre" value="editeur"> editeur
+    <INPUT type="checkbox" name="filtre" value="prix"> prix
+    <INPUT type="checkbox" name="filtre" value="categorie"> catégorie
+
+    <?php
+
+      $recherche = strtolower($_POST['recherche']);
+      $db = new PDO("mysql:host=localhost;dbname=projetifd;charset=utf8","root","");
+      $req = $db->prepare("SELECT nom,prix,editeur,nom_categorie FROM jeux INNER JOIN link_categorie_jeux ON jeux.id = link_categorie_jeux.id_jeux INNER JOIN categorie ON categorie.id = link_categorie_jeux.id_categorie  WHERE ('$recherche' = jeux.nom OR '$recherche' = jeux.editeur OR '$recherche' = jeux.prix OR '$recherche' = categorie.nom_categorie) ;");
+      $req->execute();
+      $line = $req->fetch();
+
+    $in = FALSE;
+    $tmp = NULL;
+  while($line){
+      $nom = strtolower($line['nom']);
+      $editeur = strtolower($line['editeur']);
+      $prix = strtolower($line['prix']);
+
+      $req2 = $db->prepare("SELECT nom_categorie FROM categorie INNER JOIN link_categorie_jeux ON categorie.id = link_categorie_jeux.id_categorie INNER JOIN jeux ON link_categorie_jeux.id_jeux = jeux.id WHERE jeux.nom = '$nom';");
+      $req2->execute();
+      $tmp2= $req2->fetch();
+      $categorie = $tmp2['nom_categorie'];
+      $tmp2 = $req2->fetch();
+
+      while($tmp2){
+          $categorie = $categorie . ', ' . $tmp2['nom_categorie'];
+          $tmp2 = $req2->fetch();
+      }
+
+      if( ($tmp != $nom) ){
+        $in = FALSE;
+        $tmp  = $nom;
+      }
+
+  if((!$in)){
+    ?>
+  </br></br></br></br><a href="home.php"><img src="<?=$nom?>.jpg" alt="Image" height="80" width = "80"> </a><br/>
+   <?php
+     $in = TRUE;
+     echo("<b>$nom</b></br></br>");
+     echo("Par $editeur - $prix euros</br></br>");
+     echo ("$categorie</br>");
+    }
+
+    $line = $req->fetch();
+    }
+     ?>
+
+
+
+  </body>
+</html>
