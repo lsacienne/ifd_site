aa97bf29a131744240304bb759c9d2710bb32f11
Alexandre <57919367+Zer67@users.noreply.github.com>
2020-10-30 01:31:26 +0100

    finalisation du menu déroulant
    L emenu déroulant est enfin fonctionnel (non sans mal : possibilité d'ajouter des éléments par php s'il faut)

--- header.php
+++ header.php
@@ -7,11 +7,26 @@
       echo '
         <nav>
             <ul>
-                <li class="deroulant"><a href="#">Cours Complets &ensp;</a>
+                <li><a href="#"><img src="arts/menus/menus_icone.png" width="50"> </a>
                   <ul class="sous">
-                    <li><a href="#">Cours HTML et CSS</a></li>
-                    <li><a href="#">Cours JavaScript</a></li>
-                    <li><a href="#">Cours PHP et MySQL</a></li>
+                    <li><div class="title">genre</div></li>
+                    <li><a href="#">Cartes</a></li>
+                    <li><a href="#">apéro</a></li>
+                    <li><a href="#">argent</a></li>
+                    <li><a href="#">...</a></li>
+                    
+                    <li><div class="title">durée</div></li>
+                    <li><a href="#">5-10 minutes</a></li>
+                    <li><a href="#">15-30 minutes</a></li>
+                    <li><a href="#">45 minutes - 1 heure</a></li>
+                    <li><a href="#">+ de 1 heure</a></li>
+                    
+                    <li><div class="title">âges</div></li>
+                    <li><a href="#">0-3 ans</a></li>
+                    <li><a href="#">3-7 ans</a></li>
+                    <li><a href="#">7-10 ans</a></li>
+                    <li><a href="#">10-13 ans</a></li>
+                    <li><a href="#">18 ans et +</a></li>
                   </ul>
                 </li>
             </ul>
