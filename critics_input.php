62bf17f0e9f73928067cf0ce83be0e2d784ee1ae
Alexandre <57919367+Zer67@users.noreply.github.com>
2020-11-07 18:25:09 +0100

    css de l'ajout de critiques
    Le css de l'ajout de critique est opérationnel.

--- critics_input.php
+++ critics_input.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="en" dir="ltr" class="cover">
   <head>
     <meta charset="utf-8">
     <title>Critiques</title>
@@ -8,51 +8,55 @@
     <?php
       include 'header.php';
     ?>
-    <h1>Saisir critique</h1><br/>
-    <form method="post" action="add_critic.php">
+    <div class="corps">
+        <div class="rubrique">
+            <div class="titre_rubrique">Saisir critique</div><br/>
+            <form method="post" action="add_critic.php">
 
-      <label for="jeux">Jeux:</label>
-      <select id="jeux" name="jeux">
-      <?php
-        $db = new PDO("mysql:host=localhost;dbname=projetifd;charset=utf8","root","");
-        $req = $db->prepare("SELECT nom,id FROM jeux;");
-        $req->execute();
-        $line = $req->fetch();
+              <label for="jeux">Jeux:</label>
+              <select id="jeux" name="jeux">
+              <?php
+                $db = new PDO("mysql:host=localhost;dbname=projetifd;charset=utf8","root","");
+                $req = $db->prepare("SELECT nom,id FROM jeux;");
+                $req->execute();
+                $line = $req->fetch();
 
-        while($line){
+                while($line){
 
-        ?>
-        <option value="<?php echo $line['id']; ?>"> <?php echo $line['nom']; ?></option>
-        <?php
-          $line = $req->fetch();
-        }
-        ?>
-        </select><br/><br/>
+                ?>
+                <option value="<?php echo $line['id']; ?>"> <?php echo $line['nom']; ?></option>
+                <?php
+                  $line = $req->fetch();
+                }
+                ?>
+                </select><br/><br/>
 
-	    <label for="titre">Titre:</label>
-	    <input name="titre" type="text" required/> <br /><br />
-
-
-      <label for="note">Note:</label>
-      <select id="note" name="note">
-      <?php
-      for ($i = 0; $i <= 10; $i++) {
-      ?>
-      <option value="<?=$i?>"><?=$i?></option>
-      <?php
-      }?>
-      </select><br/><br/>
+                <label for="titre">Titre:</label>
+                <input name="titre" type="text" required/> <br /><br />
 
 
-      <label for="content">Avis:</label><br /><br />
-	    <textarea name="content" cols="40" rows="10"></textarea><br /><br />
+              <label for="note">Note:</label>
+              <select id="note" name="note">
+              <?php
+              for ($i = 0; $i <= 10; $i++) {
+              ?>
+              <option value="<?=$i?>"><?=$i?></option>
+              <?php
+              }?>
+              </select><br/><br />
 
-      <button type="submit">Poster</button>
-    </form>
-    <?php
-      if(isset($_GET['failed'])){
-        echo("<br/>L'envoie à échoué. Réessayez !<br/>");
-      }
-    ?>
+
+              <label for="content">Avis:</label><br />
+                <textarea name="content" cols="40" rows="10"></textarea><br />
+
+                <div class="center"><button type="submit" id="submit_critique">Poster</button></div>
+            </form>
+            <?php
+              if(isset($_GET['failed'])){
+                echo("<br/>L'envoie à échoué. Réessayez !<br/>");
+              }
+            ?>
+        </div>
+    </div>
   </body>
 </html>
